a5c28b98da35d845699630902b30cdb6acb35480
SigTauDeltaPhi <167484810+SigTauDeltaPhi@users.noreply.github.com>
2024-04-20 12:40:10 -0400

    Update getData.js

--- scripts/getData.js
+++ scripts/getData.js
@@ -1,67 +1,49 @@
 var extractSheets = require('spreadsheet-to-json').extractSheets;
-
 var fs = require('fs');
 
-// Forking instructions: if you are forking this project for your own
-// fraternity, you will need to change the SPREADSHEET_ID to match the URL of
-// your new Google spreadsheet:
-var SPREADSHEET_ID = '1h6dVJKtETWX3Kr9PT6EaLu0gGavdi8Gnj4IlX155pfY';
+// Define your spreadsheet ID
+var SPREADSHEET_ID = '15anTEDyyPAnACj_YDXxMVXmee9ApJcZMkoJKvnQNIR4';
 
-var apiKey;
-try {
-  apiKey = fs.readFileSync('local-api-key.txt', 'utf-8').trimRight('\n');
-} catch (e) {
-  var url = 'https://github.com/revolunet/spreadsheet-to-json';
-  console.error('You need an API key to run this. Please see ' + url
-      + ' and store this in local-api-key.txt in the project root');
-  process.exit(1);
-}
-
+// Extract data from the spreadsheet
 extractSheets({
   spreadsheetKey: SPREADSHEET_ID,
-  credentials: apiKey,
-  sheetsToExtract: ['Sheet1'],
-},
-function (err, result) {
-  if (err) {
-    console.error('Something went wrong');
-    console.log(err.message);
-    console.log(err.stack);
-    process.exit(1);
-  }
-
-  // We only need the data from Sheet1.
-  result = result.Sheet1;
-  result = result.map(function (bro) {
-    if (bro.familyStarted) {
-      bro.familystarted = bro.familyStarted;
-      delete bro.familyStarted;
-    }
-    // Un-stringify booleans.
-    Object.keys(bro).forEach(function (key) {
-      if (bro[key] === 'TRUE') {
-        bro[key] = true;
-      }
-    });
-    // Remove empty fields as a storage and readability optimization.
-    Object.keys(bro).forEach(function (key) {
-      if (bro[key] === null || bro[key] === undefined) {
-        delete bro[key];
-      }
-    });
-    return bro;
-  });
-
-  var str = 'var brothers = ' + JSON.stringify(result, undefined, 2) + ';\n';
-  // Turn this into a node module that we can `require()` for testing.
-  str += "if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {\n"
-       + '  module.exports = brothers;\n'
-       + '}\n';
-  fs.writeFileSync('relations.js', str);
+  credentials: process.env.GOOGLE_SHEETS_API_KEY, // Assuming you've stored the API key as an environment variable
+  sheetsToExtract: ['Sheet1'], // Modify this to match your sheet name
 })
+  .then(function (result) {
+    // We only need the data from Sheet1.
+    var brothers = result.Sheet1;
+
+    // Adjust property names if necessary
+    brothers = brothers.map(function (bro) {
+      if (bro.familyStarted) {
+        bro.familystarted = bro.familyStarted;
+        delete bro.familyStarted;
+      }
+      // Convert 'TRUE' strings to booleans
+      Object.keys(bro).forEach(function (key) {
+        if (bro[key] === 'TRUE') {
+          bro[key] = true;
+        }
+      });
+      // Remove empty fields
+      Object.keys(bro).forEach(function (key) {
+        if (bro[key] === null || bro[key] === undefined) {
+          delete bro[key];
+        }
+      });
+      return bro;
+    });
+
+    // Convert the result to a JavaScript file
+    var jsData = 'var brothers = ' + JSON.stringify(brothers, undefined, 2) + ';\n';
+
+    // Write the data to a file
+    fs.writeFileSync('relations.js', jsData);
+
+    console.log('Data extracted and saved successfully to relations.js');
+  })
   .catch(function (err) {
-    console.error('Something went wrong');
-    console.log(err.message);
-    console.log(err.stack);
+    console.error('Error extracting data from spreadsheet: ' + err.message);
     process.exit(1);
   });
